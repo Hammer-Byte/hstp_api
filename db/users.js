5599cc91b26c5e1ce03f5d5c1c0de55a805e1964
Abhishek Jadav <abhijadav99@gmail.com>
2026-04-09 02:21:03 +0530

    Fix Query Issue

--- db/users.js
+++ db/users.js
@@ -2,7 +2,7 @@ import { executeSQLQuery } from "../libs/db.js";
 const { logger } = require("@hammerbyte/utils");
 
 export async function getUsers() {
-    return await executeSQLQuery((sql) => sql`SELECT USER_PROFILE.*, USERS.* FROM USERS LEFT JOIN USER_PROFILE ON USER_PROFILE.user_id = USERS.id ORDER BY USERS.id ASC`)
+    return await executeSQLQuery((sql) => sql`SELECT USERS.*, USER_PROFILE.company, USER_PROFILE.address, USER_PROFILE.image FROM USERS LEFT JOIN USER_PROFILE ON USERS.id = USER_PROFILE.user_id ORDER BY USERS.id ASC`)
         .then((result) => [...result])
         .catch((error) => {
             logger.error(`getUsers: ${error}`);
@@ -11,7 +11,7 @@ export async function getUsers() {
 }
 
 export async function getUserById(id) {
-    return await executeSQLQuery((sql) => sql`SELECT USERS.*, USER_PROFILE.company, USER_PROFILE.address, USER_PROFILE.image  FROM USERS JOIN USER_PROFILE ON USERS.id = USER_PROFILE.user_id WHERE USERS.id=${id}`)
+    return await executeSQLQuery((sql) => sql`SELECT USERS.*, USER_PROFILE.company, USER_PROFILE.address, USER_PROFILE.image FROM USERS JOIN USER_PROFILE ON USERS.id = USER_PROFILE.user_id WHERE USERS.id=${id}`)
         .then((result) => (result.length ? result[0] : null))
         .catch((error) => logger.error(`getUserById: ${error}`));
 }
@@ -42,13 +42,9 @@ export async function updateUserAndProfileById(id, body) {
     const userId = Number(id);
     const { email, full_name, phone, active, company, address, image } = body;
     try {
-        const usersResult = await executeSQLQuery((sql) =>
-            sql`UPDATE USERS SET email=${email}, full_name=${full_name}, phone=${phone}, active=${active} WHERE USERS.id=${userId}`,
-        );
+        const usersResult = await executeSQLQuery((sql) => sql`UPDATE USERS SET email=${email}, full_name=${full_name}, phone=${phone}, active=${active} WHERE USERS.id=${userId}`);
         if (!usersResult?.affectedRows) return null;
-        await executeSQLQuery((sql) =>
-            sql`UPDATE USER_PROFILE SET company=${company}, address=${address}, image=${image} WHERE USER_PROFILE.user_id=${userId}`,
-        );
+        await executeSQLQuery((sql) => sql`UPDATE USER_PROFILE SET company=${company}, address=${address}, image=${image} WHERE USER_PROFILE.user_id=${userId}`);
         return true;
     } catch (error) {
         logger.error(`updateUserAndProfileById: ${error}`);
